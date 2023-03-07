af117cfb5df3fe6077cc7b16104a1bb826025a63
SY1706203 <liuzhuang@buaa.edu.cn>
2023-03-07 09:06:03 +0800

    upload files

---
+++ run.sh
@@ -0,0 +1,2 @@
+#python main.py --modeltype BC_LOSS_batch --dataset amazon-book.new --n_layers 2 --neg_sample -1
+python main.py --modeltype BC_LOSS_batch --dataset tencent.new --n_layers 2 --neg_sample -1
